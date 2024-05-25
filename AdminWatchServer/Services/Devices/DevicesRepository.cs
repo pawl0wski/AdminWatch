f13d0e13b64941d189db4b1d74e206f9cec014a0
Jakub Pawłowski <59766830+pawl0wski@users.noreply.github.com>
2024-05-25 14:54:05 +0200

    Connect Shutdown button to shutdown method in client

--- AdminWatchServer/Services/Devices/DevicesRepository.cs
+++ AdminWatchServer/Services/Devices/DevicesRepository.cs
@@ -16,25 +16,4 @@ public class DevicesRepository(AdminWatchContext context) : IDevicesRepository
         => context.Devices
             .Include(dev => dev.Info)
             .ToList();
-
-    public List<Device> GetAllConnectedDevices()
-        => GetAllDevicesWithoutMeasures().Where(d => d.IsConnected()).ToList();
-
-    public List<DeviceCpuUtilization> GetLastCpuUtilization(Guid deviceId)
-        => context.Devices
-            .Include(d => d.CpuUtilizations)
-            .First(d => d.Id == deviceId)
-            .CpuUtilizations.ToList();
-
-    public List<DeviceMemoryOccupy> GetLastMemoryOccupies(Guid deviceId)
-        => context.Devices
-            .Include(d => d.MemoryOccupies)
-            .First(d => d.Id == deviceId)
-            .MemoryOccupies.ToList();
-
-    public double GetTotalMemory(Guid deviceId)
-        => context.Devices
-            .Include(d => d.Info)
-            .First(d => d.Id == deviceId)
-            .Info.TotalMemory;
 }
--- AdminWatchServer/Services/Devices/IDevicesRepository.cs
+++ AdminWatchServer/Services/Devices/IDevicesRepository.cs
@@ -7,12 +7,5 @@ public interface IDevicesRepository
     public Device GetDevice(Guid deviceId);
     public List<Device> GetAllDevicesWithoutMeasures();
 
-    public List<Device> GetAllConnectedDevices();
-
-    public List<DeviceCpuUtilization> GetLastCpuUtilization(Guid deviceId);
-    
-    public List<DeviceMemoryOccupy> GetLastMemoryOccupies(Guid deviceId);
-
-    public double GetTotalMemory(Guid deviceId);
 }
 
